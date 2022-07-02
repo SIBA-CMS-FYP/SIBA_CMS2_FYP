b6818ba450dd0fbde98df1fbc85f66ad78b3f2fe
Suraksha <surakshak571@gmail.com>
2022-07-02 13:52:36 +0500

    profile done

--- lib/routes/profile.dart
+++ lib/routes/profile.dart
@@ -6,53 +6,102 @@ class profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   
  return Scaffold(
+      body: Column(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.lightBlue, Colors.blueAccent]
+                  )
+              ),
+              child: Container(
+                width: double.infinity,
+                height: 350.0,
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
 
-   appBar: AppBar(
-         backgroundColor: Colors.blue,
-         title: Text("Profile"),
-        leading: IconButton(
-          onPressed: (){
-          Navigator.pop(context);
-        },
-         icon: Icon(
-          Icons.arrow_back_ios,
-
-        ),
-        ),
-      ),
-
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 15.0, bottom: 18.0, right: 50.0, left: 30.0),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    height: 200.0,
-                    width: 200.0,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: new AssetImage("assets/images/profile.jpg"),
-                           fit: BoxFit.cover
-                      )
-                    ),
+                      CircleAvatar(
+                         backgroundImage: new AssetImage("assets/images/profile.jpg" ),
+                         radius: 60.0,
+                       ),
+                      Text(
+                        "Suraksha",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.white,
+                        ),
+                      ), 
+                     
+                    ],
                   ),
+                ),
+              )
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30.0,horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+            TextFormField(
+    textInputAction: TextInputAction.newline,
+    keyboardType: TextInputType.multiline,
+    decoration: InputDecoration(
+      labelText: "Name",
+     labelStyle: TextStyle(color: Colors.grey ),
+     
+        border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10))
+        )
+    ),
+            ),
+            SizedBox(
+            height: 20.0,
+          ),
+             TextFormField(
+    textInputAction: TextInputAction.newline,
+    keyboardType: TextInputType.multiline,
+    decoration: InputDecoration(
+      labelText: "Email",
+     labelStyle: TextStyle(color: Colors.grey ),
+     
+        border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10))
+        )
+    ),
+            ),
+
+            SizedBox(
+            height: 20.0,
+          ),
+             TextFormField(
+    textInputAction: TextInputAction.newline,
+    keyboardType: TextInputType.multiline,
+    decoration: InputDecoration(
+      labelText: "Contact",
+     labelStyle: TextStyle(color: Colors.grey ),
+     
+        border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10))
+        )
+    ),
+            ),
                   
                 ],
               ),
             ),
-          ],
+          ),
+    
           
-        ),
-
-        
+        ],
       ),
-      
     );
   }
 }
