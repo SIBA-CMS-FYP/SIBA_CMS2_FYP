8b8c0484e1f0a2c4e01ca41a9b8ce95b5de8876c
hansraj <hansraj.bcsf18@iba-suk.edu.pk>
2022-08-03 15:02:12 +0500

    pushing main

--- lib/routes/result.dart
+++ lib/routes/result.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class result extends StatelessWidget {
+class result extends StatefulWidget {
   const result({Key? key}) : super(key: key);
 
+  @override
+  State<result> createState() => _resultState();
+}
+
+class _resultState extends State<result> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
